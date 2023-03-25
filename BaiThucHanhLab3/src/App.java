d4b15000b4797baf5f3426852e8a136ddbf3de83
DOTIENNHUAN <124874970+DOTIENNHUAN@users.noreply.github.com>
2023-03-25 18:35:47 +0700

    21/03/2023

--- BaiThucHanhLab3/src/App.java
+++ BaiThucHanhLab3/src/App.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-       Student sd = new Student();
-      sd.nhapThongtin();
-      sd.tinhTuoi();
-      sd.inThongtin();
+        Hinhtron ht = new Hinhtron();
+        Student sd = new Student();
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("1. Hinh tron\n2. Student\nLua chon cua ban: "); int luachon = sc.nextInt();
+        switch(luachon){
+            case 1:
+                ht.nhapBanKinh();
+                ht.tinhChuVi();
+                ht.tinhDienTich();
+                ht.inKetQua();
+                break;
+            case 2:
+                sd.nhapThongtin();
+                sd.tinhTuoi();
+                sd.inThongtin();
+                break;
+            default:
+                System.out.println("Vui long lua chon dung!");
+        }
+    sc.close();
     }
 }
