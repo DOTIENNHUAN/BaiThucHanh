9829d40634449fbd3e85fc3ab025e11d85273b93
DOTIENNHUAN <124874970+DOTIENNHUAN@users.noreply.github.com>
2023-05-13 16:56:34 +0700

    del

--- BaiThucHanhLab4/src/App.java
+++ BaiThucHanhLab4/src/App.java
@@ -1,13 +1,5 @@
-import java.util.ArrayList;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        ArrayList<String> arr = new ArrayList<>();
-        arr.add("Chó");
-        arr.add("Mèo");
-        arr.add("Gà");
-        arr.add("Lợn");
-        
-        System.out.println(arr.get(1));
+        System.out.println("Hello, World!");
     }
 }
