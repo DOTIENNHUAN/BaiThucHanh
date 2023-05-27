519a7b60d1941a6b10a86677fa0b6aab6fe45a98
DOTIENNHUAN <124874970+DOTIENNHUAN@users.noreply.github.com>
2023-05-27 21:55:23 +0700

    BTH 23/05/2023

---
+++ BaiThucHanhLab5/src/Slide/Slide12b.java
@@ -0,0 +1,18 @@
+package Slide;
+
+public class Slide12b {
+    public static void main(String[] args) {
+        try{
+            int data = 25;
+            if(data % 2 != 0){
+                System.out.println(data + "is odd number");
+                return;
+            }
+        }catch(ArithmeticException e){
+            System.out.println(e);
+        }finally{
+            System.out.println("finally block always executed");
+        }
+        System.out.println("rest of the code...");
+    }
+}
---
+++ BaiThucHanhLab5/src/Slide/Slide2.java
@@ -0,0 +1,8 @@
+package Slide;
+
+public class Slide2 {
+    public static void main(String[] args) {
+       int[] arr = new int[5];
+       int x = arr[5];
+    }
+}
